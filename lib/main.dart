78d26c822c30dba1b92558a5a6460de4766a5f29
Ham <jhm010234@naver.com>
2025-08-30 22:41:16 +0900

    Feat: Recap:

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Player {
+  String name;
+  Player({required this.name})
+}
+
 void main() {
+  var nico = Player(name : "potato");
+  nico.name;
   runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My Flutter App')),
+        body: Center(child: Text('Hello, World!;')),
+      ),
+    );
+  }
 }
